c81f0329b31a913756edcbe8c84744bc722d5021
t4sks <alexnovo2004@mail.ru>
2026-02-12 10:50:37 +0700

    Final Version

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ type ProjectWork struct {
 	name       string
 	nameOfWork string
 	date       time.Time
+	typeOfwork string
 }
 
 func main() {
@@ -20,7 +21,7 @@ func main() {
 	fmt.Print("Введите строку в формате: имя студента(кавычки), название темы(кавычки), дата выдачи(гггг.мм.дд)\n")
 	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 	//fmt.Println(input)
-	split := regexp.MustCompile(`"[^"]*"|\d{4}\.\d{2}\.\d{2}`).FindAllString(input, -1)
+	split := regexp.MustCompile(`"[^"]*"|\d{4}\.\d{2}\.\d{2}|[ab]`).FindAllString(input, -1)
 	for i := 0; i < len(split); i++ {
 		fmt.Println(split[i])
 	}
@@ -30,8 +31,9 @@ func main() {
 		name:       split[0],
 		nameOfWork: split[1],
 		date:       date,
+		typeOfwork: split[3],
 	}
-	fmt.Println(work.name + " " + work.nameOfWork + " " + work.date.Format("2006.01.02"))
+	fmt.Println("\n" + work.name + " " + work.nameOfWork + " " + work.date.Format("2006.01.02") + " " + work.typeOfwork + "\n")
 	fmt.Println(work)
 }
 
